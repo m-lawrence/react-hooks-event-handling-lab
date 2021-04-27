498043dadc752073c1afd173946e5dda30bffda9
Melissa Lawrence <mlawrence537@gmail.com>
2021-04-27 17:31:43 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,7 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+    return (
+        <button onFocus={event => console.log("Good!")} onBlur={event => console.log("Hey! Eyes on me!")} >Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,7 @@
-// Code Keypad Component Here
+function Keypad() {
+    return (
+        <input type="password" onChange={event => console.log("Entering password...")}></input>
+    )
+}
+
+export default Keypad;
